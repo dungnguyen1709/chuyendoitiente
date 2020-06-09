9c74714f981fb2b3b6087408af6af4e670d213ac
dungnguyen1709 <lethanhnghi135@yahoo.com>
2020-06-09 15:00:08 +0700

    hoan thanh

---
+++ chuyendoi.js
@@ -0,0 +1,14 @@
+function convert() {
+    let amount = document.getElementById("amount").value;
+    let fromCurrency = document.getElementById("fromCurrency").value;
+    let toCurrency = document.getElementById("toCurrency").value;
+    let result = 0;
+    if (fromCurrency === "USD" && toCurrency === "VND") {
+        result = amount * 23000 + " " + toCurrency;
+    } else {
+       if (fromCurrency === "VND" && toCurrency === "USD");
+       result = amount / 23000 + " " + toCurrency;
+    }
+    document.getElementById("result").innerText = result;
+
+}
